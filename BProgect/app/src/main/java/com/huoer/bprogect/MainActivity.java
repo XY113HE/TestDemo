441374f97268e0467422bdfa089ea7239ba05ec6
dllo <dllo@liumingyu.local>
2017-03-08 09:24:24 +0800

    create protect

---
+++ BProgect/app/src/main/java/com/huoer/bprogect/MainActivity.java
@@ -0,0 +1,17 @@
+package com.huoer.bprogect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * 来自龙组族长  Mr.L
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
